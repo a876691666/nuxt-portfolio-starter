d5dd14b3ab5129f9208e683c557004486f2e9cf9
Ajia0 <928200728@qq.com>
2025-03-06 09:29:08 +0000

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-console.log("github pages!!!", process.env.GITHUB_ACTIONS) // 输出环境变量
-
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -15,6 +13,6 @@ export default defineNuxtConfig({
     glsl: true,
   },
   app: {
-    baseURL: process.env.GITHUB_ACTIONS ? '/my-nuxt-project/' : '/', // 判断是否在 GitHub Actions 环境中
+    baseURL: process.env.GITHUB_PAGES ? '/my-nuxt-project/' : '/', // 判断是否在 GitHub Actions 环境中
   },
 })
